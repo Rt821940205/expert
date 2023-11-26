636fab12d1cd68c70344aaabfa75e6b79f5ab3b2
Rt821940205 <821940205@qq.com>
2023-11-26 10:01:05 +0800

    工商代理修改

--- vue.config.js
+++ vue.config.js
@@ -2,8 +2,9 @@ module.exports = {
 	devServer: {
 		proxy: {
 			'/': {
-				target: 'http://dev.heidutech.com:18681',
+				// target: 'http://dev.heidutech.com:18681',
 				// target: 'http://172.16.13.156',
+				target: 'https://paper.zjgsu.edu.cn',
 				changeOrigin: true,
 			}
 		},
